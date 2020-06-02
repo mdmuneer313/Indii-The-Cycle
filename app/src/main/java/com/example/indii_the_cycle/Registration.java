3e6bbd70048b0a1a14e6edab528914d66d053b3a
muneer <jamesbondmuneer@gmail.com>
2020-06-02 13:46:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/indii_the_cycle/Registration.java
@@ -0,0 +1,14 @@
+package com.example.indii_the_cycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
